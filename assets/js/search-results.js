584147ddc4c144bb82678d87c6a0638d2459260b
kazuya <kazuya@melty.co.jp>
2025-12-11 22:39:55 +0900

    chore: 閲覧履歴順でソートできるようにする

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -35,7 +35,7 @@
         const filter = getSearchFilter(params);
         const searchParams = new URLSearchParams({
             filter,
-            order: params.order === 'newest' ? 'published_at DESC' : 'page_view_count DESC',
+            order: params.order === 'newest' ? 'published_at DESC' : 'popularity DESC',
         });
         await displaySearchResults(searchParams);
     }
