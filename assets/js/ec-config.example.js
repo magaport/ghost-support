30ffcd9e8c7e79e2f3c798b157c00628bf9bfb47
FUJITA Kazuya <40567261+ayuzaka@users.noreply.github.com>
2026-07-02 10:20:59 +0900

    chore: URL設定はconfigに記載

---
+++ assets/js/ec-config.example.js
@@ -0,0 +1,7 @@
+window.EC_CONFIG = {
+  MP_FMS_JS_URL: 'http://localhost:18081',
+  MP_FMS_CART_URL: 'http://localhost:18083',
+  MP_BASE_API_URL: 'http://localhost:18082',
+  MP_TKM_URL: 'http://localhost:18080',
+  MP_MEDUSA_URL: 'http://localhost:9000',
+};
